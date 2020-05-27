d1fc3de8049a5a270201c1a224a4b93e2f6d4683
jialiecheng <1248212019@qq.com>
2020-05-27 21:51:09 +0800

    d222

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const headConf = require("./config/headConf.js");
 const pluginsConf = require("./config/pluginsConf.js");
 // const navConf = require("./config/navConf.js");
-// const nav = require("./nav"); //自动生成侧边栏
+const nav = require("./nav"); //自动生成侧边栏
 // const sidebarConf = require("./config/sidebarConf.js");
 
 module.exports = {
