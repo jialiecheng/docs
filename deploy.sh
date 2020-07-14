6985e25a7cdd2d7bd82d84dd4e755eec4706ba12
jialiecheng <1248212019@qq.com>
2020-07-14 14:20:07 +0800

    7-14-2

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jialiecheng/docs.git master:gh-pages
+#git push -f git@github.com:jialiecheng/docs.git master:gh-pages
+
+git push -f https://${GITHUB_TOKEN}@github.com:jialiecheng/docs.git master:gh-pages
 
 cd -
